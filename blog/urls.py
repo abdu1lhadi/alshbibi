fcae100fc4a22cd0a189f9599c8611db40482f7e
Abdu1lhadi <abxxdo@gmail.com>
2019-06-06 04:27:13 +0300

    add new templates

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,14 @@ from django.views.generic import TemplateView
 urlpatterns =[
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('detail/<int:post_id>/', views.post_detail, name='detail'),
-    path('test1/', TemplateView.as_view(template_name='blog/index.html'), name='test'),
+    path('coffee/', views.coffee, name='coffee'),
+    path('course/', views.course, name='course'),
+    path('europe/', views.europeClassc, name='europe'),
+    path('external/', views.external, name='external'),
+    path('interior/', views.interior, name='interior'),
+    path('women/', views.specialWomen, name='women'),
+    path('dyes/', views.dyes, name='dyes'),
+    path('upgrading/', views.upgrading, name='upgrading'),
+    path('allcars/', views.allcars, name='allcars'),
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -22,25 +22,31 @@ def home(request):
 def about(request):
     return render(request, 'blog/about.html', {'title': 'ABOUT US'})
 
-def post_detail(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
-    
+def coffee(request):
+    return render(request, 'blog/coffee_shop.html', {'title': 'coffee'})
 
-    #check before save data from comment form
-    if request.method == 'POST':
-        comment_form = NewCustomers(data=request.POST)
-        if comment_form.is_valid():
-            new_comment = comment_form.save(commit=False)
-            new_comment.post = post
-            new_comment.save()
-            comment_form = NewCustomers()
-    else:
-        comment_form = NewCustomers()
-    
-    context = {
-        'title': post,
-        'post': post,
-        'comment_form': comment_form,
-    }
-    
-    return render(request, 'blog/detail.html', context)
+def course(request):
+    return render(request, 'blog/course_malaysia.html', {'title': 'course'})
+
+def europeClassc(request):
+    return render(request, 'blog/Eur_a_classc.html', {'title': 'europeClassc'})
+
+def external(request):
+    return render(request, 'blog/External_design.html', {'title': 'ABOUT US'})
+
+def interior(request):
+    return render(request, 'blog/ineterior_decorations.html', {'title': 'interior'})
+
+def specialWomen(request):
+    return render(request, 'blog/special_women.html', {'title': 'specialWomen'})
+
+def dyes(request):
+    return render(request, 'blog/specialty_dyes.html', {'title': 'dyes'})
+
+def upgrading(request):
+    return render(request, 'blog/upgrading _engines.html', {'title': 'upgrading'})
+
+def allcars(request):
+    return render(request, 'blog/allcars.html', {'title': 'allcars'})
+
+
